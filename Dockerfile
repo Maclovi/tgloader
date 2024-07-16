2bfe2e88b2afa9b700512f527f1b353917b4a222
Sergey Yavorsky <yavorskyserge@gmail.com>
2024-07-16 19:48:27 +0300

    add dockerfile & impove auth.py

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+from python:3.12.4-slim
+
+WORKDIR /app
+
+COPY . .
+
+RUN apt-get update &&\
+    apt-get install -y git &&\
+    python3.12 -m pip install --upgrade pip &&\
+    python3.12 -m pip install .
+
+CMD ["runbot"]
--- loader/auth.py
+++ loader/auth.py
@@ -1,4 +1,5 @@
 import logging
+import shutil
 import sys
 from pathlib import Path
 
@@ -11,19 +12,7 @@ from loader.tgclient.client import get_client
 logger = logging.getLogger(__name__)
 
 
-def search_session_file() -> bool:
-    return any(
-        True
-        for x in Path().iterdir()
-        if x.is_file() and x.name.endswith(".session")
-    )
-
-
-def auth_youtube() -> None:
-    if pytube.auth.is_auth():
-        logger.info("__cache__ dir is exists")
-        return
-
+def _auth_youtube() -> None:
     _ = YouTube(
         "https://youtu.be/UprwkbzUX6g?si=pWBvtXGTlTMR8QLG",
         use_oauth=True,
@@ -31,24 +20,53 @@ def auth_youtube() -> None:
     ).streams
 
 
-def auth_telegram() -> None:
-    if search_session_file():
-        logger.info("Session file is exists")
-        return
+def auth_youtube() -> None:
+    tokens = Path(".sens/tokens.json").resolve()
+    path_yt = Path(pytube.auth.__file__).parent.resolve()
+    if tokens.exists():
+        logger.info("coping tokens.json to pytube dir base")
+        (path_yt / "__cache__").mkdir()
+        shutil.copy(tokens, path_yt / "__cache__/tokens.json")
+    else:
+        logger.info("Authorize user youtube account")
+        _auth_youtube()
 
-    client_config = load_config().tg_client
-    client = get_client(client_config)
+
+def _auth_telegram() -> None:
+    client = get_client(conf.tg_client)
     with client:
         client.loop.run_until_complete(
             client.send_message("me", "hello, myself!")
         )
 
 
+def auth_telegram() -> None:
+    session_file = Path(".sens/me.session")
+    if session_file.exists():
+        logger.info("coping session file to base dir")
+        shutil.copy(session_file, ".")
+    else:
+        logger.info("Authorize user telegram account")
+        _auth_telegram()
+
+
+def check_cache_pytube() -> bool:
+    base = Path(pytube.auth.__file__).parent.resolve()
+    return (base / "__cache__" / "tokens.json").exists()
+
+
+def check_session_telegram() -> bool:
+    return Path("me.session").resolve().exists()
+
+
 def auth_all() -> None:
-    auth_youtube()
-    auth_telegram()
+    if not check_cache_pytube():
+        auth_youtube()
+    if not check_session_telegram():
+        auth_telegram()
 
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+    conf = load_config()
     auth_all()
