7a188ed5bd6219686cc1a3bb9cfdbfe3672861cd
Sergey <yavorskyserge@gmail.com>
2024-08-16 19:46:05 +0300

    fix middlewares

--- loader/tgbot/middlewares/common.py
+++ loader/tgbot/middlewares/common.py
@@ -3,7 +3,7 @@ from collections.abc import Awaitable, Callable
 from typing import TYPE_CHECKING, Any, cast
 
 from aiogram import BaseMiddleware
-from aiogram.types import Message, TelegramObject
+from aiogram.types import Message, TelegramObject, User
 
 if TYPE_CHECKING:
     from loader.ioc import Container
@@ -19,16 +19,19 @@ class ThrottlingMiddleware(BaseMiddleware):
         data: dict[str, Any],
     ) -> Any:
         msg = cast(Message, event)
+        user_id = cast(User, msg.from_user).id
 
         ioc: "Container" = data["ioc"]  # noqa: UP037
         client_id = ioc.config.tg_ids.client_id
         user = f"user{msg.chat.id}"
         userttl = await ioc.redis.ttl(user)
 
-        if userttl > 0 and msg.chat.id != client_id:
+        if userttl > 0:
             await msg.answer(f"Stop flooding, repeat after {userttl}")
             return
 
-        await ioc.redis.set(user, "0", ex=15)
+        if user_id != client_id:
+            await ioc.redis.set(user, "0", ex=15)
+
         logger.info(f"Do processing by {user}")
         return await handler(msg, data)
